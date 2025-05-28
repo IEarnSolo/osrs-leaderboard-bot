82a22399b015b14d2755e41a7048a2e7ae72c9b9
IEarnSolo <rlgithub@yahoo.com>
2025-05-28 05:29:47 -0400

    Converted numerous files to work with ES modules

--- src/commands/setchannel.js
+++ src/commands/setchannel.js
@@ -1,18 +1,19 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { GuildSettings } from '../utils/database.js';
 
-export const data = new SlashCommandBuilder()
-  .setName('setchannel')
-  .setDescription('Set the leaderboard channel to the current channel.');
+export default {
+  data: new SlashCommandBuilder()
+    .setName('setchannel')
+    .setDescription('Set the leaderboard channel to the current channel.'),
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const channelId = interaction.channel.id;
 
-export async function execute(interaction) {
-  const guildId = interaction.guild.id;
-  const channelId = interaction.channel.id;
+    await GuildSettings.upsert({
+      guildId,
+      leaderboardChannelId: channelId,
+    });
 
-  await GuildSettings.upsert({
-    guildId,
-    leaderboardChannelId: channelId,
-  });
-
-  await interaction.reply(`Leaderboard channel set to <#${channelId}>.`);
-}
+    await interaction.reply(`Leaderboard channel set to <#${channelId}>.`);
+  },
+};
--- src/commands/updateplayers.js
+++ src/commands/updateplayers.js
@@ -1,23 +1,23 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { GuildSettings } from '../utils/database.js';
 
-export const data = new SlashCommandBuilder()
-  .setName('updateplayers')
-  .setDescription('Set the interval (in hours) for updating player data.')
-  .addIntegerOption(option =>
-    option.setName('interval')
-      .setDescription('Update interval in hours')
-      .setRequired(true)
-  );
+export default {
+  data: new SlashCommandBuilder()
+    .setName('updateplayers')
+    .setDescription('Set the interval (in hours) for updating player data.')
+    .addIntegerOption(option =>
+      option.setName('interval')
+        .setDescription('Update interval in hours')
+        .setRequired(true)),
+  async execute(interaction) {
+    const guildId = interaction.guild.id;
+    const interval = interaction.options.getInteger('interval');
 
-export async function execute(interaction) {
-  const guildId = interaction.guild.id;
-  const interval = interaction.options.getInteger('interval');
+    await GuildSettings.upsert({
+      guildId,
+      updateIntervalHours: interval,
+    });
 
-  await GuildSettings.upsert({
-    guildId,
-    updateIntervalHours: interval,
-  });
-
-  await interaction.reply(`Player update interval set to ${interval} hour(s).`);
-}
+    await interaction.reply(`Player update interval set to ${interval} hour(s).`);
+  },
+};
