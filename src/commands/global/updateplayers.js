a65621ae904d15516e4719452016c9970849c152
IEarnSolo <rlgithub@yahoo.com>
2025-06-02 04:22:54 -0400

    Fix import directory in setchannel.js and updateplayers.js

--- src/commands/global/updateplayers.js
+++ src/commands/global/updateplayers.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { GuildSettings } from '../utils/database.js';
+import { GuildSettings } from '../../utils/database.js';
 
 export default {
   data: new SlashCommandBuilder()
