0ac8b63ca4f510ee8930af5c3aca651b182532a8
IEarnSolo <rlgithub@yahoo.com>
2025-06-06 03:29:51 -0400

    Fix length of description

--- src/commands/global/updateplayers.js
+++ src/commands/global/updateplayers.js
@@ -4,7 +4,7 @@ import { GuildSettings } from '../../utils/database.js';
 export default {
   data: new SlashCommandBuilder()
     .setName('updateplayers')
-    .setDescription('Set the interval (in hours) for auto-updating Wise Old man profiles of members with Leaderboard role.')
+    .setDescription('Set the interval (in hours) for auto-updating Wise Old Man profiles of members with Leaderboard role')
     .addIntegerOption(option =>
       option.setName('interval')
         .setDescription('Update interval in hours')
