68d641ff17f706a5517bbf19740c151649259326
IEarnSolo <rlgithub@yahoo.com>
2025-06-06 17:19:23 -0400

    Fix named export of womClient

--- src/utils/womClient.js
+++ src/utils/womClient.js
@@ -1,9 +1,7 @@
 import { WOMClient } from '@wise-old-man/utils';
 import 'dotenv/config';
 
-const womClient = new WOMClient({
+export const womClient = new WOMClient({
   apiKey: process.env.WOM_API_KEY,
   userAgent: 'iearnsolo',
 });
-
-export default womClient;
